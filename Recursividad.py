24335a80c3be7a49965e2ef978b4bd1ee0656503
Yiorch0102 <129303743+Yiorch0102@users.noreply.github.com>
2023-09-26 12:46:04 -0600

    Update Recursividad.py

--- Recursividad.py
+++ Recursividad.py
@@ -1,22 +1,23 @@
+
 def fibonacci_recursivo(n):
     if n == 0:
         resultado = 0
-    elif n == 1:
+    elif n == 1:       
         resultado = 1
     else:
         resultado = fibonacci_recursivo(n-1) + fibonacci_recursivo(n-2)
     return resultado
-    
-print(fibonacci_recursivo(8))
-
-
+i= int(input("Hasta que posición te gustaría imprimir fibonacci"))
+for n in range (i): 
+    print(fibonacci_recursivo(n + 1))
+print("")
 
 def factorial_recursivo(n):
     if n == 1 or n == 0:
         resultado = 1
     else:
          resultado = n * factorial_recursivo(n-1)
-
     return resultado
-
-print(factorial_recursivo(4))
+i = int(input("Escribe el número factorial"))
+for n in range (i):
+    print(factorial_recursivo(n+1))
